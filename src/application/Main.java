28b6a5a9d2e1bfe818b2226f02937e12e6bacda0
davidjaneiro92 <davidjaneiro92@gmail.com>
2020-11-11 16:17:06 -0300

    DepartamentoService

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,38 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+		
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	public static Scene getMainScene() {
+        return mainScene;
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,12 @@
+package model.services;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class DepartmentService  {
+	
+
+
+}
